829eec2c58d5470de5dd71d840ab5d56646bc776
Scaffolder <scaffolder@backstage.io>
2024-06-01 22:20:42 -0300

    initial commit

---
+++ src/main/java/com/sulimann/picpay/utils/constants/Path.java
@@ -0,0 +1,11 @@
+package com.sulimann.picpay.utils.constants;
+
+public final class Path {
+
+    public static final String INIT = "/init";
+
+    private Path() {
+        throw new AssertionError("Não é permitido instanciar esta classe.");
+    }
+
+}
